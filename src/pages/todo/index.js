a7d8c039d34d1159f931dafb293e5b83d1ce28af
JiHye0214 <qkrwlgp1526@gmail.com>
2023-11-08 17:10:19 +0900

    231108 : git commit react practice2

--- src/pages/todo/index.js
+++ src/pages/todo/index.js
@@ -1,18 +1,52 @@
 import styled from "styled-components";
 import { flexCenter } from "../../styles/common";
+import { useState } from "react";
+import AddTodoModal from "./components/addTodoModal";
 
 const TodoPage = () => {
-    return(
-        <Wrapper>
-            <h1>Todopage 입니다</h1>
-        </Wrapper>
-    )
-}
+  const [addModalState, setAddModal] = useState(false);
+
+  const onClickAddBTN = () => {
+    setAddModal((prev) => !prev);
+
+  };
+
+  return (
+    <>
+    {addModalState && <AddTodoModal closeBTN={setAddModal}/>}
+      <Wrapper>
+        <Container>
+          <h1>Todopage</h1>
+          <h5>Write something to do</h5>
+          <AddTodoButton onClick={onClickAddBTN}>
+            Write something to do
+          </AddTodoButton>
+          <h4>
+            test <input type="text"></input>
+          </h4>
+        </Container>
+      </Wrapper>
+    </>
+  );
+};
 
 export default TodoPage;
 
 const Wrapper = styled.div`
-    height : calc(100vh - 330px);
-    ${flexCenter}
-    background-color: silver;
-`
+  width: 100%;
+  height: calc(100vh - 330px);
+  ${flexCenter}
+  flex-direction: column;
+  background-color: silver;
+`;
+
+const Container = styled.div`
+  background-color: white;
+  height: 500px;
+  width: 700px;
+  text-align: center;
+`;
+
+const AddTodoButton = styled.button`
+  padding: 7px;
+`;
